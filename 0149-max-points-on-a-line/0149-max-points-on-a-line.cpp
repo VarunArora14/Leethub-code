8651701246365cff9cc161ec39c9a91e3a682ad4
Varun Arora <56734311+VarunArora14@users.noreply.github.com>
2023-01-08 11:45:24 +0530

    Time: 23 ms (98.00%), Space: 13.4 MB (54.05%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,48 +1,39 @@
 class Solution {
 public:
-
-int maxPoints(vector<vector<int>> &points) {
-    int result = 0;
-    for(int i = 0; i < points.size(); i++){
-        int samePoint = 1;
-        unordered_map<double, int> map;
-        for(int j = i + 1; j < points.size(); j++){
-            if(points[i][0] == points[j][0] && points[i][1] == points[j][1]){
-                samePoint++;
+    int maxPoints(vector<vector<int>>& points) {
+        
+        int n=points.size();
+        if(n<=2) return n;
+        
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            unordered_map<double, int> slopeVsPointsMap;
+            int samePoint=1;
+            for(int j=i+1;j<n;j++)
+            {
+                if(points[i][0] == points[j][0])
+                {
+                    slopeVsPointsMap[INT_MAX]++; // inf slope when dx=0 from dy/dx
+                }
+                else if(points[i][1] == points[j][1] && points[i][0] == points[j][0])
+                {
+                    samePoint++;
+                }
+                else{
+                    double slope = double(points[i][1] - points[j][1]) / double(points[i][0] - points[j][0]);
+                    slopeVsPointsMap[slope]++;
+                }
             }
-            else if(points[i][0] == points[j][0]){
-                map[INT_MAX]++;
+            
+            int maxLocalSameSlope=0;
+            for(auto [key,val]: slopeVsPointsMap)
+            {
+                maxLocalSameSlope = max(maxLocalSameSlope, val);
             }
-            else{
-                double slope = double(points[i][1] - points[j][1]) / double(points[i][0] - points[j][0]);
-                map[slope]++;
-            }
+            maxLocalSameSlope+=samePoint;
+            ans = max(ans, maxLocalSameSlope);
         }
-        int localMax = 0;
-        for(auto it = map.begin(); it != map.end(); it++){
-            localMax = max(localMax, it->second);
-        }
-        localMax += samePoint;
-        result = max(result, localMax);
+        return ans;
     }
-    return result;
-}
 };
-
- /*
-     *  A line is determined by two factors,say y=ax+b
-     *  
-     *  If two points(x1,y1) (x2,y2) are on the same line(Of course). 
-
-     *  Consider the gap between two points.
-
-     *  We have (y2-y1)=a(x2-x1),a=(y2-y1)/(x2-x1) a is a rational, b is canceled since b is a constant
-
-     *  If a third point (x3,y3) are on the same line. So we must have y3=ax3+b
-
-     *  Thus,(y3-y1)/(x3-x1)=(y2-y1)/(x2-x1)=a
-
-     *  Since a is a rational, there exists y0 and x0, y0/x0=(y3-y1)/(x3-x1)=(y2-y1)/(x2-x1)=a
-
-     *  So we can use y0&x0 to track a line;
-     */
