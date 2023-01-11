3ce3939c4dca92e4c2b6399dc1f65fe1d01c482b
Varun Arora <56734311+VarunArora14@users.noreply.github.com>
2023-01-11 17:04:43 +0530

    Time: 223 ms (68.46%), Space: 65.5 MB (32.21%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
-    int helper(int node, vector<bool> &hasApple, vector<vector<int>> &adj, vector<int> &vis)
-{
-  vis[node] = true;
-
-  int childTime = 0, ans = 0;
-  for (auto nbr : adj[node])
-  {
-    // ans+=helper(nbr, hasApple, ans);
-    if (!vis[nbr])
+    int dfs(int src, vector<bool>& hasApple, vector<int>& vis, vector<vector<int>>& adj)
     {
-      childTime = helper(nbr, hasApple, adj, vis);
-
-      if (childTime || hasApple[nbr])
-        ans += childTime + 2;
+        int nbrSum=0, totalSum=0;
+        vis[src]=true;
+        
+        // go till last node with no nbrs to go to
+        for(auto nbr: adj[src])
+        {
+            if(!vis[nbr])
+            {
+                nbrSum = dfs(nbr, hasApple, vis, adj); // sum of each nbr of src node
+                
+                // for leafs, nbrSum=0, for apples we add +2 and also for backtracking while nbrSum contains sum from
+                // nbr to leaf. Check apple of each nbr and return sum based on that
+                if(hasApple[nbr] || nbrSum) totalSum+=nbrSum+2;
+            }
+        }
+        vis[src]=false;
+        return totalSum;
     }
-  }
-
-  vis[node] = false;
-  return ans;
-}
-int minTime(int n, vector<vector<int>> &edges, vector<bool> &hasApple)
-{
-  vector<vector<int>> adj(n);
-  vector<int> vis(n);
-
-  for (auto edge : edges)
-  {
-    int u = edge[0];
-    int v = edge[1];
-
-    adj[u].push_back(v);
-    adj[v].push_back(u);
-  }
-  return helper(0, hasApple, adj, vis);
-  // return ans;
-}
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        vector<vector<int>> adj(n);
+        for(auto edge: edges)
+        {
+            int u=edge[0];
+            int v=edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<int> vis(n);
+        // if Apple at 0, the ans is 0 as we dont have to traverse
+        return dfs(0,hasApple, vis, adj);
+    }
 };
-
-// whenever we find apple, return true from that position and add that as 1 in ans while backtracking
+// each time we reach an apple, we want to return the ans from that position and if nbr as an apple, it is in path
+// so add childSum+2 while backtracking
