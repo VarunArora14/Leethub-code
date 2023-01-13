db694e0449ec66a175492bbfa2872d5ef51838b0
Varun Arora <56734311+VarunArora14@users.noreply.github.com>
2023-01-13 15:51:45 +0530

    Time: 785 ms (51.83%), Space: 237.8 MB (16.14%) - LeetHub

---
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -0,0 +1,72 @@
+class Solution {
+    
+    pair<int , int> solve(vector<int> adj[] , string &s , int par , int &ans , vector<bool> &vis)
+    {
+        priority_queue<int> pq ;
+        
+        vis[par] = 1 ;
+        
+        for(auto &x: adj[par])
+        {
+            if(!vis[x])
+            {
+                 pair<int , int> p1 = solve(adj , s , x , ans , vis) ;
+                 if(p1.first != s[par]) // adjacent same char check
+                 {
+                     pq.push(-p1.second) ;
+                     if(pq.size() > 2) pq.pop() ; // keep longest 2 lengths as think of them as left and right branches
+                     // of a string which lead to ans
+                 }
+            }
+        }
+        
+        int mxnum = 0;
+        int curmx = 0 ;
+        
+        if(pq.size() > 0)
+        {
+            int f = -pq.top() ;
+            pq.pop() ;
+            curmx+= f ; // store max length for each node
+            
+            mxnum = max(mxnum , f) ; // store longest length node to return 
+        }
+            
+          if(pq.size() > 0)
+        {
+            int f = -pq.top() ;
+            pq.pop() ;
+            curmx+= f ;
+              
+             mxnum = max(mxnum , f) ;
+        }
+        
+        curmx++ ; // add current node to ans with branches
+        ans = max(ans,  curmx) ;
+        
+        return {s[par] , mxnum + 1 } ; // return the child character to check for adjacent 
+        // and maxnum+1 is longest length we return as we decide later which length to size using pq
+            
+    }
+    
+public:
+    int longestPath(vector<int>& parent, string s) {
+        // NOTE - we just dont want adjacent chars to be same, we can multiple occurances in same string tho
+        int n = parent.size() ;
+        vector<int> adj[n] ;
+        
+        for(int i=1; i<n ; i++)
+        {
+            adj[i].push_back(parent[i]) ;
+            adj[parent[i]].push_back(i) ;
+
+        }
+
+        vector<bool > vis(n , 0) ;
+        
+        int ans = 0;
+        
+        solve(adj , s , 0 , ans, vis) ;
+        return ans ;
+    }
+};
