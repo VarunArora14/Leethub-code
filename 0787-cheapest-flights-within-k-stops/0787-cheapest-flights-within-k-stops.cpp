33c9abc155535f2a3b64fd1401d3627c29179513
Varun Arora <56734311+VarunArora14@users.noreply.github.com>
2023-01-26 16:42:52 +0530

    Time: 79 ms (18.36%), Space: 12.6 MB (95.82%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,63 +1,24 @@
-using vi =vector<int>;
-using vpii = vector<pair<int,int>>;
 class Solution {
 public:
-    
-    void dfs(int src, int dest, int k, int cost, unordered_map<int, vpii> &mp, int& ans, vi &vis)
-{
-  // base cases
-  if (src == dest)
-  {
-    ans = cost;
-    return;
-  }
-  if (k == 0)
-    return;
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        // Distance from source to all other nodes.
+        vector<int> dist(n, numeric_limits<int>::max());
+        dist[src] = 0;
 
-  vis[src] = 1;
-
-  for (auto e : mp[src])
-  {
-    if (vis[e.first] == 0)
-    {
-      // reduce fn calls
-      if (cost + e.second > ans)
-        continue;
-        
-        // cout<<"cost: "<<cost<<"\n";
-
-      dfs(e.first, dest, k - 1, cost + e.second, mp, ans, vis);
-    }
-  }
-
-  vis[src] = 0;
-}
-    
-   int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> adj(n);
-        for (auto& e : flights) {
-            adj[e[0]].push_back({e[1], e[2]});
-        }
-        vector<int> dist(n, INT_MAX);
-        queue<pair<int, int>> q;
-        q.push({src, 0});
-        int stops = 0;
-
-        while (stops <= k && !q.empty()) {
-            int sz = q.size();
-            // Iterate on current level.
-            while (sz--) {
-                auto [node, distance] = q.front();
-                q.pop();
-                // Iterate over neighbors of popped node.
-                for (auto& [neighbour, price] : adj[node]) {
-                    if (price + distance >= dist[neighbour]) continue;
-                    dist[neighbour] = price + distance;
-                    q.push({neighbour, dist[neighbour]});
+        // Run only K+1 times since we want shortest distance in K hops.
+        for (int i = 0; i <= k; i++) {
+            // Create a copy of dist vector.
+            vector<int> temp(dist);
+            for (auto& flight : flights) {
+                if (dist[flight[0]] != numeric_limits<int>::max()) {
+                    temp[flight[1]] = min(temp[flight[1]], dist[flight[0]] + flight[2]);
                 }
             }
-            stops++;
+            // Copy the temp vector into dist.
+            dist = temp;
         }
-        return dist[dst] ==INT_MAX ? -1 : dist[dst];
+        return dist[dst] == numeric_limits<int>::max() ? -1 : dist[dst];
     }
 };
+
+// bellman for approach
