467e3e000fdc2b565b73669c47d5bdec586d8d6c
Varun Arora <56734311+VarunArora14@users.noreply.github.com>
2023-11-03 09:55:40 +0530

    Time: 44 ms (25.45%), Space: 16.2 MB (76.57%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -1,19 +1,19 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
         
-        st = [i for i in range(1,n+1)]
-        print(st)
+        # st = [i for i in range(1,n+1)]
+        # print(st)
         res=[]
         
         c=0
         i=0
         while i<len(target):
-            if st[c] == target[i]:
+            if c+1 == target[i]:
                 i+=1
                 c+=1
                 res.append("Push")
             else:
-                while target[i] > st[c]:
+                while target[i] > c+1:
                     res.append("Push")
                     res.append("Pop")
                     c+=1
