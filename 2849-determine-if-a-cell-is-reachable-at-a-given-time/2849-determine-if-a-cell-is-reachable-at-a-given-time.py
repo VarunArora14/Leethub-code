b796d6c138dc9082478da9dfe4219014ca556052
Varun Arora <56734311+VarunArora14@users.noreply.github.com>
2023-11-09 01:24:08 +0530

    Time: 39 ms (67.84%), Space: 16.3 MB (42.87%) - LeetHub

--- 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.py
+++ 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.py
@@ -7,3 +7,11 @@ class Solution:
             return t != 1
 
         return x_dist <= t and y_dist <= t  
+    
+    # reason we need max of xdist, ydist is that consider n*m matrix and try to go diagonally
+    # we can minimize distance on going diagonally which is x,y => x+1.y+1
+    # now,to get to destination, we have to make sure larger value should be considered as threshold, consider 3X4 matrix
+    # from top left to bottom right (takes 4 seconds)
+    # Also consider horizontal and vertical cases as well(lines)
+    
+    # base cases can exist for fx=sx and sy=fy which ans is 1
