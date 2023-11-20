23897a7cea9a9ceff874e6cd7c75c9813b8b0f65
Varun Arora <56734311+VarunArora14@users.noreply.github.com>
2023-11-21 00:37:33 +0530

    Time: 440 ms (5.01%), Space: 17.4 MB (12.17%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        start=0
+        pos=start
+        end=len(s)
+
+        store=set()
+
+        substrCount=1
+        while pos<end:
+            print(pos)
+            if s[pos] in store:
+                substrCount+=1
+                print(f"substr -> start: {start}, end: {pos-1}")
+                start=pos
+                store.clear()
+            else:    
+                store.add(s[pos])
+                pos+=1
+        
+        return substrCount
