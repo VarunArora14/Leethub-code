117fb6efb7c767fe9241da0dc8114c45c19086d6
Varun Arora <56734311+VarunArora14@users.noreply.github.com>
2023-12-21 13:05:12 +0530

    Time: 770 ms (42.26%), Space: 59.4 MB (34.52%) - LeetHub

---
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+        points.sort()
+        
+        print(points)
+        
+        n  = len(points)
+        
+        ans = 0
+        for i in range(1,n):
+            ans = max(ans, points[i][0] - points[i-1][0])
+        
+        return ans
