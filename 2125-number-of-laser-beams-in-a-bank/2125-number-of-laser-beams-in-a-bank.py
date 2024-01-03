6e8974292a81561e3aa8a221ba65b3d683f2cc2b
Varun Arora <56734311+VarunArora14@users.noreply.github.com>
2024-01-03 13:51:47 +0530

    Time: 367 ms (5.01%), Space: 19.6 MB (6.86%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -1,31 +1,17 @@
-# if a row has 1 or more security devices, find for each row by going down if there exists a beam or not
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
-        if len(bank)==1:
+        su = 0
+        a = []
+        for i in range(len(bank)):
+            count = 0
+            for j in range(len(bank[i])):
+                if bank[i][j]=='1':
+                    count+=1
+            if count>0:
+                a.append(count) # only append next row if count > 0, avoids problem where next row had 0 devices
+        if len(a)<=1:
             return 0
-
-        bank.reverse()
-
-        strlen = len(bank[0])
-        rowFlagArr = [False for _ in range(strlen)]
-
-        res=0
-
-        for row in range(len(bank)):
-            string = bank[row]
-            currRowDevices=0
-            currRowDevices = string.count("1")
-            for i in rowFlagArr:
-                # security device found at 
-                if i==True:
-                    res+=currRowDevices # all devices in current row get added if any next row
-            if currRowDevices > 0:
-                currRowFlagArr = [False for _ in range(strlen)]
-                for i in range(len(string)):
-                    if string[i]=="1":
-                        currRowFlagArr[i]=True
-                    else:
-                        currRowFlagArr[i] = False
-                rowFlagArr = currRowFlagArr
-
-        return res
+        else:
+            for i in range(len(a)-1):
+                su+=a[i]*a[i+1]
+        return su
