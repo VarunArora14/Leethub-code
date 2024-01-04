f7e1410c04e090f767ffde28b734b4a5f2e2cafb
Varun Arora <56734311+VarunArora14@users.noreply.github.com>
2024-01-04 12:40:11 +0530

    Time: 549 ms (94.24%), Space: 31.5 MB (18.18%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
@@ -16,5 +16,10 @@ class Solution:
             elif val%3==2:
                 res+=1+(val-2)/3
             else:
-                res+=2 + (val-4)/3 # for cases val>=4, we find if we substract 4, we get multiple of 3
+                res+=2 + (val-4)/3 # works for cases val>=4, we find if we substract 4, we get multiple of 3
         return int(res)
+# since we consider val%3 before, consider cases where val%3==1 => [4,7,11,13,16,19,22..]
+# in all the elements, notice if you reduce 4 from it, you will get a multiple of 3
+# they way it works is that as val%3==1 => val-1%3==0 => but we cannot substract 1 from it and so, as the number
+# is in form 3n+1, when we do 3n+1-4, we get 3(n-1) which is a multiple of 3
+# and since we want to find as many 3 pairs for minimum operations, above logic works well
