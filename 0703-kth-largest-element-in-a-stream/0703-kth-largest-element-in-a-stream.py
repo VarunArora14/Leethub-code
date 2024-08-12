90d198037e961b0b37aa2bde792029cdbd7f09e5
Varun Arora <56734311+VarunArora14@users.noreply.github.com>
2024-08-12 14:11:05 +0530

    Time: 79 ms (59.09%), Space: 20.5 MB (53.25%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
@@ -3,20 +3,22 @@ import heapq
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.nums = []
-        for num in nums:
-            heapq.heappush(self.nums, num)
-        self.k=k
-
+        self.k = k
+        self.heap = nums
+        heapq.heapify(self.heap) # heapify rather than push each time
+        while len(self.heap) > k:
+            heapq.heappop(self.heap)
+        
     def add(self, val: int) -> int:
-        heapq.heappush(self.nums, val)
-        while len(self.nums)>self.k:
-            heapq.heappop(self.nums)
-        kth_element = heapq.heappop(self.nums)
-        heapq.heappush(self.nums,kth_element)
-        return kth_element
-
+        if len(self.heap) < self.k or val > self.heap[0]:
+            heapq.heappush(self.heap, val) # add to heap only when new element is bigger than smallest element at heap[0]
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
+
+
+# after heapify, the array[0] will have the smallest element and the print of whole array will be in heap format
