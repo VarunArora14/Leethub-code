556986987063c8b087db2117d88a265c2847e556
Varun Arora <56734311+VarunArora14@users.noreply.github.com>
2024-08-16 12:56:34 +0530

    Time: 1311 ms (91.38%), Space: 42.6 MB (51.44%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.py
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.py
@@ -1,18 +1,26 @@
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
-        min_vals = []
-        max_vals = []
-        
-        for i,arr in enumerate(arrays):
-            max_vals.append((arr[-1],i))
-            min_vals.append((arr[0],i))
-        
-        # print(len(max_vals))
-        max_vals.sort(key=lambda x: -x[0])
-        min_vals.sort(key=lambda x: x[0])
-        
-        if max_vals[0][1] == min_vals[0][1]:
-            return max(max_vals[0][0] - min_vals[1][0], max_vals[1][0] - min_vals[0][0])
-        else:
-            return max_vals[0][0] - min_vals[0][0]
+        min_val, max_val = float('inf'), float('-inf')
+        min_index, max_index = -1, -1
+
+        for i, arr in enumerate(arrays):
+            if arr[0] < min_val:
+                min_val = arr[0]
+                min_index = i
+            if arr[-1] > max_val:
+                max_val = arr[-1]
+                max_index = i
+
+        if min_index != max_index:
+            return max_val - min_val
+
+        second_max_val, second_min_val = float('-inf'), float('inf')
+
+        for i, arr in enumerate(arrays):
+            if i != min_index:
+                second_min_val = min(second_min_val, arr[0])
+            if i != max_index:
+                second_max_val = max(second_max_val, arr[-1])
+
+        return max(max_val - second_min_val, second_max_val - min_val)
             
