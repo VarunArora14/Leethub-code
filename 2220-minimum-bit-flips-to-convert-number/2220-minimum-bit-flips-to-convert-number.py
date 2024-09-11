5c5b3593f4122e5d2bfd8a7c8da7f4279609e91b
Varun Arora <56734311+VarunArora14@users.noreply.github.com>
2024-09-11 12:01:26 +0530

    Time: 38 ms (37.69%), Space: 16.5 MB (26.96%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -3,9 +3,10 @@ class Solution:
         
         # when we do xor of 2 numbers, same bits become 0 and diff bits stay 1
         xor_val = start ^ goal
-        print(bin(xor_val)[2:])
+        
         counter=0
-        for x in bin(xor_val)[2:]:
-            if x=='1':
+        while xor_val:
+            if xor_val & 1:
                 counter+=1
+            xor_val = xor_val>>1
         return counter
