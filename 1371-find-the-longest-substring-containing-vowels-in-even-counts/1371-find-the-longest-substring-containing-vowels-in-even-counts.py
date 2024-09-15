e128920f582cac64787b758079de4b64e2a573d6
Varun Arora <56734311+VarunArora14@users.noreply.github.com>
2024-09-15 10:49:03 +0530

    Time: 198 ms (89.69%), Space: 22.2 MB (33.15%) - LeetHub

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.py
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -1,17 +1,13 @@
 class Solution:
     def findTheLongestSubstring(self, s: str) -> int:
-        prefixXOR = 0
-        characterMap = [0] * 26
-        characterMap[ord("a") - ord("a")] = 1
-        characterMap[ord("e") - ord("a")] = 2
-        characterMap[ord("i") - ord("a")] = 4
-        characterMap[ord("o") - ord("a")] = 8
-        characterMap[ord("u") - ord("a")] = 16
-        mp = [-1] * 32
-        longestSubstring = 0
-        for i in range(len(s)):
-            prefixXOR ^= characterMap[ord(s[i]) - ord("a")]
-            if mp[prefixXOR] == -1 and prefixXOR != 0:
-                mp[prefixXOR] = i
-            longestSubstring = max(longestSubstring, i - mp[prefixXOR])
-        return longestSubstring
+        for i in range(len(s),0,-1):
+            for j in range(len(s)-i+1):
+                l=(s[j:j+i])
+                odd = 0
+                for k in "aeiou":
+                    if(l.count(k)%2!=0):
+                        odd = True
+                        break
+                if(not odd):
+                    return (i)
+        return 0
